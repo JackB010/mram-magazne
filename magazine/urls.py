5c25eba56113bff029125ab3aac44130b823663b
jack bill <jackbill687@gmail.com>
2021-08-24 21:55:52 +0100

    fix

--- magazine/urls.py
+++ magazine/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
-from django.views.generic.base import RedirectView
 from django.contrib.sitemaps.views import sitemap
 from posts.sitemaps import PostSitemap
 from posts.views import about_us, policy
@@ -13,8 +12,7 @@ sitemaps = {'posts': PostSitemap,}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', include('profiles.urls')),
-    path('', RedirectView.as_view(url='posts/')),
-    path('posts/', include('posts.urls')),
+    path('', include('posts.urls')),
     path("about_us/", about_us, name='about_us'),
     path("policy/", policy, name='policy'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
